2476f34f60b6340d090d170a9d69f3c32eabda9b
mottelz <shachna@gmail.com>
2022-02-17 18:53:56 -0500

    Class and exercise 01

---
+++ class01/data-and-ops.py
@@ -0,0 +1,19 @@
+# This is a comment which means it won't be read by python
+
+# Data types
+5  # Int
+5.4  # Float
+'Hello'  # String
+True  # Boolean
+
+# Math operators
+5 + 4
+5 - 4
+5 * 4
+5 / 4
+
+# Variables
+
+myVar = input('Enter a number: ')
+
+print(myVar + 5)
---
+++ class01/exercise01.py
@@ -0,0 +1,5 @@
+# 1. Create two variables with int values
+
+# 2. Add the two together and store the value in a third variable
+
+# 3. Print out all three variables
