a019b6655defae27dad662b50e29534ffabd326f
mottelz <shachna@gmail.com>
2022-02-17 20:38:32 -0500

    exercise done

--- class01/exercise02.py
+++ class01/exercise02.py
@@ -1,6 +1,12 @@
 # 1. Create a list with at least three numbers on it
+a = [12, 3, 4546, 809, 190324, 875432098]
 
 # 2. Compare the first and last number in your list and store the resulting boolean in a variable
+b = a[0] > a[-1]
+
 
 # 3. Write an if statement that uses your boolean from step 2. If it's true print out a happy message otherwise print out a sad one.
-
+if b:
+	print('hooray!')
+else:
+	print('boo hoo :(')
